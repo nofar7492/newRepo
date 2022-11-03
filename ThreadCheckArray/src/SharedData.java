14fa4db0365518df405ee0cb19adc965be7a4abb
Nofar.Oshri.Bensimon <Nofar.Oshri.Bensimon@e.braude.ac.il>
2022-11-03 13:10:28 +0200

    111

--- ThreadCheckArray/src/SharedData.java
+++ ThreadCheckArray/src/SharedData.java
@@ -1,13 +1,16 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public class SharedData 
 {
-	private int [] array;
+	private ArrayList<Integer> array;
 	private boolean [] winArray;
 	private boolean flag;
 	private final int b;
 	
-	public SharedData(int[] array, int b) {
+	public SharedData(ArrayList<Integer> array2, int b) {
 		
-		this.array = array;
+		this.array = array2;
 		this.b = b;
 	}
 
@@ -21,7 +24,7 @@ public class SharedData
 		this.winArray = winArray;
 	}
 
-	public int[] getArray() 
+	public ArrayList<Integer> getArray() 
 	{
 		return array;
 	}
